4d36124b9548a774c7f0418416418a91b5d9c9e5
Jeremy Hirschler <JJ.Hirschler@gmail.com>
2022-07-06 20:16:55 -0400

    initial commit

---
+++ first-jsx/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello Dojo!</h1>
+      </header>
+      <main className = "App-main">
+        <h2>Things I need to do:</h2>
+        <ul>
+          <li>Learn react</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
