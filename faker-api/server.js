5aee0711ba1e17e2929d9f8714c1715adfd41f12
Jeremy Hirschler <JJ.Hirschler@gmail.com>
2022-07-29 14:20:05 -0400

    initial commit

---
+++ faker-api/server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+const {faker} = require('@faker-js/faker');
+
+app.listen(PORT, () => {
+    console.log(`Server live on ${PORT}`)
+})
+
+const createUser = () => {
+    const user = {
+        password: faker.internet.password(),
+        email: faker.internet.email(),
+        phoneNumber: faker.phone.number(),
+        lastName: faker.name.lastName(),
+        firstName: faker.name.firstName(),
+        _id: faker.datatype.uuid()
+    }
+    return user;
+}
+
+const createCompany = () => {
+    company = {
+        _id: faker.datatype.uuid(),
+        name: faker.company.companyName(),
+        address: {
+            street: faker.address.street(),
+            city: faker.address.city(),
+            state: faker.address.state(),
+            zipCode: faker.address.zipCode(),
+            country:faker.address.country(),
+        }
+    }
+    return company;
+}
+
+app.get('/api/user/new', (req, res) => {
+    const user = createUser();
+    res.json(user);
+})
+
+app.get('/api/companies/new', (req, res) => {
+    const company = createCompany();
+    res.json(company);
+})
+
+app.get('/api/user/company', (req, res) => {
+    const user = createUser();
+    const company = createCompany();
+    const userPlusCompany = {
+        user,
+        company
+    };
+    res.json(userPlusCompany);
+})
