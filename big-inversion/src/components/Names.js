4641419ce8e61ff99ed5b59f37aafc49da671b7f
Jeremy Hirschler <JJ.Hirschler@gmail.com>
2022-08-24 18:36:58 -0400

    updated to include method

--- big-inversion/src/components/Names.js
+++ big-inversion/src/components/Names.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react'
 
 class Names extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            newAge: this.props.age
+        }
+    }
+    increaseAge = (age) => {
+        console.log('hi')
+        this.setState({newAge: age+1})
+    }
+
+
+
+
     render(){
-        const {firstName, lastName, age, hairColor} = this.props
+        const {firstName, lastName, hairColor} = this.props
         return <div className='nameCard'>
             <h1>{lastName}, {firstName}</h1>
-            <p>Age: {age}</p>
+            <p>Age: {this.state.newAge}</p>
             <p>Hair Color: {hairColor}</p>
+            <button onClick = {()=>this.increaseAge(this.state.newAge)}>Birthday button for {firstName} {lastName}</button>
         </div>
     }
 }
